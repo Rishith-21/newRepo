6ea0d55f76f051dc5101a05b994e4e10e4f7484f
Rishith-21 <shettyrishith2005@gmail.com>
2025-06-29 11:48:50 +0530

    Initial Commit

---
+++ Duplicate.py
@@ -0,0 +1,8 @@
+# Define a list of numbers with duplicates
+numbers = [1, 2, 2, 3, 4, 4, 5]
+
+# Convert the list to a set to remove duplicates, then convert it back to a list
+unique_numbers = list(set(numbers))
+
+# Print the list without duplicates
+print("List without duplicates:", unique_numbers)
